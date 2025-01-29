0d7675b2d4ca5b9231ccc855a05c48c15bc2bae9
vijay <vv16062004@gmail.com>
2025-01-29 09:20:54 +0530

    The day 7 is done

--- src/day7.java
+++ src/day7.java
@@ -1,5 +1,26 @@
+import java.util.*;
 public class day7 {
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[][] arr = new int[n][n];
+        int num=1;
+        for(int i=0; i<n;i++){
+
+            for(int j=0;j<n-i;j++){
+                arr[j][i] = num;
+//                System.out.print(arr[i][j]);
+                num++;
+            }
+            System.out.println();
+        }
+        for(int i=0; i<n;i++) {
+
+            for (int j = 0; j < n - i; j++) {
+                System.out.print(arr[i][j]+ " ");
+            }
+            System.out.println();
+        }
 
     }
 }
